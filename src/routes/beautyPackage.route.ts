6222298a5eb5d1a15f0f2f417f6786f54891b0fb
ariful171042 <arif171042@gmail.com>
2023-10-08 04:58:32 +0600

    beauty package controller added

--- src/routes/beautyPackage.route.ts
+++ src/routes/beautyPackage.route.ts
@@ -1,24 +1,43 @@
 import express, { Router } from 'express';
 import AuthController from '../controllers/auth.controller';
+import AuthMiddleware from '../middlewares/auth.middelware';
+import BeautyPackageController from '../controllers/beutyPackage.controller';
 
 const beautyPackageRouter: Router = express.Router();
+const authInstance = new AuthMiddleware();
+const beautyPackageInstance = new BeautyPackageController();
 
 //get all beauty packages
-beautyPackageRouter.get('/');
+beautyPackageRouter.get('/', beautyPackageInstance.getAllBeautyPackages);
 
 //get all bookings
 beautyPackageRouter.get('/read');
 
 //get a beautypackage
-beautyPackageRouter.get('/:bid');
+beautyPackageRouter.get('/:bid', beautyPackageInstance.getAnBeautyPackage);
 
 //create a beauty package
-beautyPackageRouter.get('/create');
+beautyPackageRouter.get(
+  '/create',
+  authInstance.isAuthhenticated,
+  authInstance.isAdmin,
+  beautyPackageInstance.createABeautyPackage
+);
 
 //update a beauty package
-beautyPackageRouter.put('/:bid');
+beautyPackageRouter.put(
+  '/:bid',
+  authInstance.isAuthhenticated,
+  authInstance.isAdmin,
+  beautyPackageInstance.updateABeautyPackage
+);
 
 //delete a beauty package
-beautyPackageRouter.delete('/:bid');
+beautyPackageRouter.delete(
+  '/:bid',
+  authInstance.isAuthhenticated,
+  authInstance.isAdmin,
+  beautyPackageInstance.deleteABeautyPackage
+);
 
 export default beautyPackageRouter;
